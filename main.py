4fb9215ab52ee01f0cabb18d3ee1be89c3030012
chanaho <kyc222222@gmail.com>
2026-06-17 07:31:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,4 @@ def root():
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    return {
-        "result": "AI 서버 정상 연결 (YOLO 미연결 상태)"
-    }
+    return {"result": "AI 서버 정상"}
